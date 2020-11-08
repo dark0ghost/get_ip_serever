bd4cc72fec98475b2da2fc322f1bd7f83d59df31
dark0ghost <dark0ghostworkmail@seznam.cz>
2020-11-08 23:12:04 +0300

    start

---
+++ src/handler.rs
@@ -0,0 +1,5 @@
+pub(crate) struct Handler;
+
+impl Handler{
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use mio::Events;
+use std::net::TcpListener;
+
+
+mod handler;
+mod settings;
+use settings::Settings;
+use handler::Handler;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(128);
+    const SETTINGS: Settings = Settings::new();
+    const HANDLER: Handler = Handler;
+    let mut server = TcpListener::bind(SETTINGS)?;
+
+
 }
